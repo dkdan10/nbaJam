790c8a995ceeef7f428f0d9090f80a0cb17aa969
Daniel Keinan <danielkeinan0@gmail.com>
2019-08-12 20:42:22 -0400

    Initial commit for NBAJam Javascript Project

---
+++ webpack.dev.js
@@ -0,0 +1,12 @@
+import merge from "webpack-merge";
+import common from "./webpack.common.js";
+
+module.exports = merge(common, {
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: "./",
+        watchContentBase: true,
+        open: "Google Chrome"
+    }
+});
