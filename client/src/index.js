e9ceef5998e6483982123136bf13d9fc1820769a
Daniel Keinan <danielkeinan0@gmail.com>
2019-08-21 12:06:23 -0400

    Prevent scrolling with arrowkeys

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,13 @@ import NBAJamGame from './js/game.js'
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('nba-jam-game');
     const canvasBackground = document.getElementById('nba-jam-background');
+
+    // Prevent scrolling with arrow keys
+    window.addEventListener("keydown", function (e) {
+        // space and arrow keys
+        if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+        }
+    }, false);
     new NBAJamGame(canvas, canvasBackground);
 });
